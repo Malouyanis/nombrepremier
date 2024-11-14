e9aacabd791a31b32c2a4311353470e926f416d2
Malouyanis <yanis.malou@edu.esiee.fr>
2024-11-14 10:31:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-from math import sqrt
-
-#### Fonction secondaire
-
+"""Module pour vérifier si un nombre est premier."""
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
-
+    """Vérifie si un nombre entier p est un nombre premier."""
+    if p <= 1:
+        return False
+    if p == 2:
+        return True
+    if p % 2 == 0:
+        return False
+    for i in range(3, int(p**0.5) + 1, 2):
+        if p % i == 0:
+            return False
+    return True
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Affiche les nombres premiers de 0 à 99."""
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
-    print()
-
+    print()  # Pour terminer la ligne après la liste des nombres premiers
 
 if __name__ == "__main__":
     main()
